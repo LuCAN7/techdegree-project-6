48998696a6cd41bb3dba2df2a5acecb175cb7a7a
Lucan F <dapps911@hotmail.com>
2019-09-08 22:04:20 -0400

    setup all routes

--- app.js
+++ app.js
@@ -9,12 +9,43 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
 
+app.use('/static', express.static('public'));
+
+app.use((req, res, next) => {
+  // console.log('...Problem');
+  const err = new Error('Not Found');
+  // ('Oh noes, Something went wrong!');
+  next(err);
+});
+
 app.get('/', function (req, res) {
-  res.render('./index');
+
+  console.log(res.locals);
+  // res.locals = data.projects;
+  res.render('layout');
   // let dave = require('./views/index');
+  
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+  
+});
+
+app.get('/projects/:id', function (req, res) {
+  const project = res.params.id;
+  res.render('project', { project: project });
   
 })
 
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+  
+});
+
+
 app.listen(3000, () => {
-  console.log('Server is working...');
+  console.log('Server is started on PORT:3000...');
 });
