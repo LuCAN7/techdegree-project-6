5635f451ccc06226b201424524c611ed60fde1b0
Lucan F <dapps911@hotmail.com>
2019-09-17 00:03:04 -0400

    setup all views to display data

--- app.js
+++ app.js
@@ -2,66 +2,59 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('body-parser');
-const projects = require('./data.json')
+const dataJSON = require('./data.json');
 
 const app = express();
 
+// const PORT = process.env.PORT || 4000;
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug'); // what kind of files to look for
+
+//
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/static', express.static('public'));
-
-// console.dir("1-",app.locals.title);
-// console.log(projects.projects[0]);
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  // const routeParams = req.param;
-  const data = projects.projects;
-  // console.log(routeParams);
-  console.log(data);
-  // console.log('Reponse Body:', req.body);
-  // console.log('Response Locals:', req.locals);
-  // res.locals = data.projects;
-  // let message = 'Relax this is only a test'
-  res.render('index', { projects: data });
+  const projects = dataJSON.projects;
+  // console.log();
+  res.render('index', { projects: projects});
  
 });
 
 app.get('/about', function (req, res) {
   res.render('about');
- 
+  // res.send('<h2> this is the about page </h2>');
+});
+
+app.get('/project/:id', function (req, res) {
+  const projects = dataJSON.projects;
+  const id = parseInt(req.params.id) - 1;
+  
+  res.render('project', { project: projects[id] });
   
 });
 
-// app.get('/projects/:id', function (req, res) {
-//   const project = res.params.id;
-//   console.log(project)
-//   // res.render('project', { project: project });
-  
+
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   // console.log('...Problem');
+//   // ('Oh noes, Something went wrong!');
+//   err.status = 404;
+//   next(err);
 // });
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  // console.log('...Problem');
-  // ('Oh noes, Something went wrong!');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  // res.render('error');  
-});
+// app.use((err, req, res, next) => {
+//   res.locals.error = err;
+//   res.status(err.status);
+//   // res.render('error');  
+// });
 
 
-app.listen(3000, () => {
-  console.log('Server is started on PORT:3000...');
+app.listen(4000, () => {
+  console.log(`Server is started on PORT: 4000...`);
 });
 
