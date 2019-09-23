242e52e538d47248f5d9e5257061e4b0f481ae0f
Lucan F <dapps911@hotmail.com>
2019-09-22 22:05:49 -0400

    completed all project requirements

--- app.js
+++ app.js
@@ -11,23 +11,19 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug'); // what kind of files to look for
 
-//
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   const projects = dataJSON.projects;
-  // console.log();
+
   res.render('index', { projects: projects});
- 
 });
 
 app.get('/about', function (req, res) {
   res.render('about');
- 
 });
 
 app.get('/project/:id', function (req, res) {
@@ -35,27 +31,22 @@ app.get('/project/:id', function (req, res) {
   const id = parseInt(req.params.id) - 1;
   
   res.render('project', { project: projects[id] });
-  
 });
-
 
 app.use((req, res, next) => {
-  const error = new Error('Not Found');
+  const error = new Error('Page Not Found');
   
   error.status = 404;
-  console.log('Sorry....Page cannot be found!')
+  console.log('There was an Error - ', error);
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.locals.error = err;
   res.render('error');  
-  
 });
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.locals.error = error;
-  res.send('Opps! Something went Wrong...');  
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server is started on PORT: ${PORT}`);
 });
-
